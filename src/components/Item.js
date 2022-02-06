9cd62ab370173f1fdde5ffb378ef892df2f6cac4
Angel Rosario <arosario.us@gmail.com>
2022-02-06 14:40:08 -0500

    complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isClicked, setIsClicked] = useState(false)
+
+  function handleCartClick(){
+    setIsClicked(!isClicked)
+  };
+
+  const color = isClicked ? "magenta" : "yellow";
+
   return (
-    <li className="">
+    <li className={isClicked ? "in-cart" : " "}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button style={{ background: color }} className="add" onClick={handleCartClick}>{isClicked ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
